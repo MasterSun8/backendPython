d42c1f3ca01a4d167222dbba9995ac7b2d5ddd2b
MasterSun8 <kacpi.doro@gmail.com>
2023-04-25 11:10:00 +0200

    prajntuje w phytonie

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ CORS(app)
 
 @app.route("/")
 def index():
-    return "essa"
+    file = open("data.txt")
+    return file.read()
 
 @app.route("/help")
 def help():
@@ -15,3 +16,23 @@ def help():
 @app.route("/welp")
 def welp():
     return "This is a welp message"
+
+@app.route("/add")
+def add():
+    file = open("data.txt", "r+")
+    count = file.read()
+    if(count):
+        count = int(count)
+        count += 1
+        file.truncate(0)
+        file.seek(0)
+    else:
+        count = 1
+    file.write(str(count))
+    file.close()
+    return "Added success"
+
+@app.route("/clear")
+def clear():
+    file = open("data.txt", "w").close()
+    return "File cleared"
